cbbcc59ba7edb15ae525d2891f5af8ed056c9c16
katherine1408 <laudeyil@gmail.com>
2024-08-21 23:14:45 -0500

    Creacion de casillas de verificacion

--- app.py
+++ app.py
@@ -6,17 +6,18 @@ car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 print(car_data.head())
 
-hist_button = st.button('Construir Histograma')
+hist_checkbox = st.checkbox('Mostrar histograma de kilometraje')
 
-if hist_button:
+if hist_checkbox:
+    # Mensaje y creación del histograma
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig_hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
     
-    st.write('Creación de un histograma con los valores del odometro de los autos')
-    fig = px.histogram(car_data,x='odometer')
-    st.plotly_chart(fig)
-    
-scatter_button = st.button('Construir gráfico de dispersión')
+# Casilla de verificación para el gráfico de dispersión
+scatter_checkbox = st.checkbox('Mostrar gráfico de dispersión precio vs. kilometraje')
 
-if scatter_button:
+if scatter_checkbox:
     # Mensaje y creación del gráfico de dispersión
     st.write('Creación de un gráfico de dispersión de precio vs. kilometraje')
     fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Precio vs. Kilometraje")
